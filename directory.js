88eef38d56a3b7a95e9188f1c6f59a3cef7caf1c
Tom Hsu <tom268@gmail.com>
2017-10-27 21:31:46 -0700

    Added a hover to customer service to display text through directory.js

---
+++ directory.js
@@ -0,0 +1,8 @@
+function appear() {
+  //var x = document.getElementByClassName("hover");
+  document.getElementById("hover").style.visibility = "visible";
+}
+
+function disappear() {
+  document.getElementById("hover").style.visibility = "hidden";
+}
