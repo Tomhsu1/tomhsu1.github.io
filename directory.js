7900c8f6d2e481b2cc80b83e7672f8c73a68c63b
Tom Hsu <tom268@gmail.com>
2017-10-27 22:55:49 -0700

    Finished all buttons to appearing/disappearing text

--- directory.js
+++ directory.js
@@ -3,17 +3,35 @@ var filin = false;
 
 function appear(choice) {
   if (choice === 'customerSkills') {
-  document.getElementById("customer").style.visibility = "visible";
-  document.getElementById("filing").style.visibility = "hidden";
-  custom = false;
-} else if (choice === 'filing/organizing') {
-  document.getElementById("filing").style.visibility = "visible";
-  document.getElementById("customer").style.visibility = "hidden";
-  filin = false;
+    document.getElementById("customer").style.visibility = "visible";
+    document.getElementById("filing").style.visibility = "hidden";
+    custom = false;
+  } else if (choice === 'filing/organizing') {
+    document.getElementById("filing").style.visibility = "visible";
+    document.getElementById("customer").style.visibility = "hidden";
+    filin = false;
+  } else if (choice === 'html') {
+    document.getElementById("customer").style.visibility = "hidden";
+    document.getElementById("filing").style.visibility = "hidden";
+    document.getElementById("css").style.visibility = "visible";
+  } else if (choice === 'public') {
+    document.getElementById("customer").style.visibility = "hidden";
+    document.getElementById("filing").style.visibility = "hidden";
+    document.getElementById("css").style.visibility = "hidden";
+    document.getElementById("publicSpeaking").style.visibility = "visible";
+  } else if (choice === 'photoshop') {
+    document.getElementById("customer").style.visibility = "hidden";
+    document.getElementById("filing").style.visibility = "hidden";
+    document.getElementById("css").style.visibility = "hidden";
+    document.getElementById("publicSpeaking").style.visibility = "hidden";
+    document.getElementById("fcp").style.visibility = "visible";
   }
 }
 
 function disappear() {
   document.getElementById("customer").style.visibility = "hidden";
   document.getElementById("filing").style.visibility = "hidden";
+  document.getElementById("css").style.visibility = "hidden";
+  document.getElementById("publicSpeaking").style.visibility = "hidden";
+  document.getElementById("fcp").style.visibility = "hidden";
 }
