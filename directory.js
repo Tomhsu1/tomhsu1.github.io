25777de7055ddce6a8a93a6f7953b4c5c43ba298
Tom Hsu <tom268@gmail.com>
2017-10-27 22:35:49 -0700

    Made two buttons and one appears/disappears based on button

--- directory.js
+++ directory.js
@@ -1,8 +1,21 @@
-function appear() {
-  //var x = document.getElementByClassName("hover");
-  document.getElementById("hover").style.visibility = "visible";
+var custom = false;
+var filin = false;
+
+function appear(choice) {
+  if (choice === 'customerSkills') {
+  document.getElementById("customer").style.visibility = "visible";
+  document.getElementById("filing").style.visibility = "hidden";
+  custom = false;
+} else if (choice === 'filing/organizing') {
+  document.getElementById("filing").style.visibility = "visible";
+  document.getElementById("customer").style.visibility = "hidden";
+  filin = false;
+  }
 }
 
 function disappear() {
-  document.getElementById("hover").style.visibility = "hidden";
+  if (custom === false && filin === false) {
+  document.getElementById("customer").style.visibility = "hidden";
+  document.getElementById("filing").style.visibility = "hidden";
+  }
 }
